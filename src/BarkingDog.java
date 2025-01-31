ba5af92202096166d9dc38b3023c1a1677901642
Ankita <ankitasrivastav1105@gmail.com>
2025-01-31 16:51:17 +0530

    first

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -11,7 +11,6 @@ public class BarkingDog {
     }
     public static boolean shouldWakeUp(boolean barking, int hourOfDay)
     {
-        return barking && ((hourOfDay < 8 || hourOfDay > 22) && hourOfDay >= 0 && hourOfDay <= 23);
 
     }
 }
